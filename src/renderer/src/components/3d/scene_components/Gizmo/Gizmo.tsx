6d3d10008218caaf64c2ee52ef77332b75c33f44
lidaltonlima <lidaltonlima@gmail.com>
2025-08-13 13:22:09 -0400

    style: add lines in axis negatives

--- src/renderer/src/components/3d/scene_components/Gizmo/Gizmo.tsx
+++ src/renderer/src/components/3d/scene_components/Gizmo/Gizmo.tsx
@@ -1,15 +1,8 @@
-import {
-	Billboard,
-	CameraControls,
-	Circle,
-	Hud,
-	Line,
-	OrthographicCamera,
-	Text
-} from '@react-three/drei'
+import { CameraControls, Hud, OrthographicCamera } from '@react-three/drei'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useMemo, useRef } from 'react'
 import * as THREE from 'three'
+import { GizmoSphere } from './GizmoSphere'
 
 export const Gizmo = (): React.JSX.Element => {
 	const { size, camera, controls } = useThree()
@@ -17,6 +10,7 @@ export const Gizmo = (): React.JSX.Element => {
 
 	const mesh = useRef<THREE.Group>(new THREE.Group())
 
+	// Move came with click in axis ///////////////////////////////////////////////////////////////
 	const moveToUp = (): void => {
 		cameraControls.rotateTo(0, 0, true)
 	}
@@ -40,6 +34,7 @@ export const Gizmo = (): React.JSX.Element => {
 	const moveToBack = (): void => {
 		cameraControls.rotateTo(Math.PI, Math.PI / 2, true)
 	}
+	///////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Calcula a posição com base no tamanho atual
 	const position = useMemo(() => {
@@ -69,6 +64,7 @@ export const Gizmo = (): React.JSX.Element => {
 			<fog attach='fog' args={['#1a1a1a', 0.5, 1.5]} />
 			<group ref={mesh} position={position}>
 				<GizmoSphere
+					camera={camera}
 					onClickX={moveToRight}
 					onClickXNegative={moveToLeft}
 					onClickY={moveToBack}
@@ -78,221 +74,5 @@ export const Gizmo = (): React.JSX.Element => {
 				/>
 			</group>
 		</Hud>
-	)
-}
-
-interface IGizmoSphereProps {
-	onClickX?: () => void
-	onClickY?: () => void
-	onClickZ?: () => void
-	onClickXNegative?: () => void
-	onClickYNegative?: () => void
-	onClickZNegative?: () => void
-}
-
-function GizmoSphere({
-	onClickX = undefined,
-	onClickY = undefined,
-	onClickZ = undefined,
-	onClickXNegative = undefined,
-	onClickYNegative = undefined,
-	onClickZNegative = undefined
-}: IGizmoSphereProps): React.JSX.Element {
-	const labelX = useRef<THREE.MeshBasicMaterial>(null)
-	const labelY = useRef<THREE.MeshBasicMaterial>(null)
-	const labelZ = useRef<THREE.MeshBasicMaterial>(null)
-	const labelXNegative = useRef<THREE.Group>(null)
-	const labelYNegative = useRef<THREE.Group>(null)
-	const labelZNegative = useRef<THREE.Group>(null)
-
-	return (
-		<>
-			<group>
-				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#ff3653'} lineWidth={2} />
-				<Billboard position={[0.4, 0, 0]}>
-					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
-						X
-						<meshBasicMaterial ref={labelX} color={'black'} fog={false} />
-					</Text>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							labelX.current?.color.set('white')
-						}}
-						onPointerLeave={() => {
-							labelX.current?.color.set('black')
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickX) onClickX()
-						}}
-					>
-						<meshBasicMaterial color={'#ff3653'} />
-					</Circle>
-				</Billboard>
-			</group>
-			<group rotation-z={Math.PI * 0.5}>
-				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#77b316'} lineWidth={2} />
-				<Billboard position={[0.4, 0, 0]}>
-					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
-						Y
-						<meshBasicMaterial ref={labelY} color={'black'} fog={false} />
-					</Text>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							labelY.current?.color.set('white')
-						}}
-						onPointerLeave={() => {
-							labelY.current?.color.set('black')
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickY) onClickY()
-						}}
-					>
-						<meshBasicMaterial color={'#77b316'} />
-					</Circle>
-				</Billboard>
-			</group>
-			<group rotation-y={-Math.PI * 0.5}>
-				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#317acd'} lineWidth={2} />
-				<Billboard position={[0.4, 0, 0]}>
-					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
-						Z
-						<meshBasicMaterial ref={labelZ} color={'black'} fog={false} />
-					</Text>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							labelZ.current?.color.set('white')
-						}}
-						onPointerLeave={() => {
-							labelZ.current?.color.set('black')
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickZ) onClickZ()
-						}}
-					>
-						<meshBasicMaterial color={'#317acd'} />
-					</Circle>
-				</Billboard>
-			</group>
-			<group rotation-z={Math.PI}>
-				{/* <Line
-					worldUnits
-					points={[0, 0, 0, 0.31, 0, 0]}
-					color={'#ff3653'}
-					lineWidth={0.02}
-					depthTest={false}
-					depthWrite={false}
-				/> */}
-				<Billboard position={[0.4, 0, 0]}>
-					<group ref={labelXNegative} visible={false} position={[0, 0, 0.01]}>
-						<Text font='/fonts/Inter-Bold.woff' fontSize={0.14}>
-							-X
-							<meshBasicMaterial color={'white'} fog={false} />
-						</Text>
-					</group>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							if (labelXNegative.current) labelXNegative.current.visible = true
-						}}
-						onPointerLeave={() => {
-							if (labelXNegative.current) labelXNegative.current.visible = false
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickXNegative) onClickXNegative()
-						}}
-					>
-						<meshBasicMaterial color={'#ff3653'} />
-					</Circle>
-					<Circle scale={0.09} position={[0, 0, 0.005]}>
-						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
-					</Circle>
-				</Billboard>
-			</group>
-			<group rotation-z={-Math.PI / 2}>
-				{/* <Line
-					worldUnits
-					points={[0, 0, 0, 0.31, 0, 0]}
-					color={'#77b316'}
-					lineWidth={0.02}
-					depthTest={false}
-					depthWrite={false}
-				/> */}
-				<Billboard position={[0.4, 0, 0]}>
-					<group ref={labelYNegative} visible={false} position={[0, 0, 0.01]}>
-						<Text font='/fonts/Inter-Bold.woff' color={'white'} fontSize={0.14}>
-							-Y
-							<meshBasicMaterial color={'white'} fog={false} />
-						</Text>
-					</group>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							if (labelYNegative.current) labelYNegative.current.visible = true
-						}}
-						onPointerLeave={() => {
-							if (labelYNegative.current) labelYNegative.current.visible = false
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickYNegative) onClickYNegative()
-						}}
-					>
-						<meshBasicMaterial color={'#77b316'} />
-					</Circle>
-					<Circle scale={0.09} position={[0, 0, 0.005]}>
-						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
-					</Circle>
-				</Billboard>
-			</group>
-			<group rotation-y={Math.PI / 2}>
-				{/* <Line
-					worldUnits
-					points={[0, 0, 0, 0.31, 0, 0]}
-					color={'#317acd'}
-					lineWidth={0.02}
-					depthTest={false}
-					depthWrite={false}
-				/> */}
-				<Billboard position={[0.4, 0, 0]}>
-					<group ref={labelZNegative} visible={false} position={[0, 0, 0.01]}>
-						<Text font='/fonts/Inter-Bold.woff' color={'white'} fontSize={0.14}>
-							-Z
-							<meshBasicMaterial color={'white'} fog={false} />
-						</Text>
-					</group>
-					<Circle
-						scale={0.1}
-						onPointerEnter={(event) => {
-							event.stopPropagation()
-							if (labelZNegative.current) labelZNegative.current.visible = true
-						}}
-						onPointerLeave={() => {
-							if (labelZNegative.current) labelZNegative.current.visible = false
-						}}
-						onClick={(event) => {
-							event.stopPropagation()
-							if (onClickZNegative) onClickZNegative()
-						}}
-					>
-						<meshBasicMaterial color={'#317acd'} />
-					</Circle>
-					<Circle scale={0.09} position={[0, 0, 0.005]}>
-						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
-					</Circle>
-				</Billboard>
-			</group>
-		</>
 	)
 }
---
+++ src/renderer/src/components/3d/scene_components/Gizmo/GizmoSphere.tsx
@@ -0,0 +1,302 @@
+import { Billboard, Circle, Line, Text } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three'
+import { Line2, LineSegments2 } from 'three/examples/jsm/Addons'
+
+interface IGizmoSphereProps {
+	camera: THREE.Camera
+	onClickX?: () => void
+	onClickY?: () => void
+	onClickZ?: () => void
+	onClickXNegative?: () => void
+	onClickYNegative?: () => void
+	onClickZNegative?: () => void
+}
+
+export const GizmoSphere = ({
+	camera,
+	onClickX = undefined,
+	onClickY = undefined,
+	onClickZ = undefined,
+	onClickXNegative = undefined,
+	onClickYNegative = undefined,
+	onClickZNegative = undefined
+}: IGizmoSphereProps): React.JSX.Element => {
+	const labelX = useRef<THREE.MeshBasicMaterial>(null)
+	const labelY = useRef<THREE.MeshBasicMaterial>(null)
+	const labelZ = useRef<THREE.MeshBasicMaterial>(null)
+	const labelXNegative = useRef<THREE.Group>(null)
+	const labelYNegative = useRef<THREE.Group>(null)
+	const labelZNegative = useRef<THREE.Group>(null)
+	const lineXNegative = useRef<Line2 | LineSegments2>(null)
+	const lineYNegative = useRef<Line2 | LineSegments2>(null)
+	const lineZNegative = useRef<Line2 | LineSegments2>(null)
+
+	const isMouseEnter = useRef(false)
+
+	useFrame(() => {
+		if (Math.abs(camera.rotation.x) < 0.001 && Math.abs(camera.rotation.y) === 0) {
+			// Top ////////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = true
+			if (lineXNegative.current) lineXNegative.current.visible = true
+
+			if (labelYNegative.current) labelYNegative.current.visible = true
+			if (lineYNegative.current) lineYNegative.current.visible = true
+		} else if (
+			Math.abs(camera.rotation.x) > Math.PI - 0.001 &&
+			Math.abs(camera.rotation.y) === 0
+		) {
+			// Bottom /////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = true
+			if (lineXNegative.current) lineXNegative.current.visible = true
+
+			if (labelYNegative.current) labelYNegative.current.visible = true
+			if (lineYNegative.current) lineYNegative.current.visible = true
+
+			if (labelZNegative.current) labelZNegative.current.visible = true
+			if (lineZNegative.current) lineZNegative.current.visible = true
+		} else if (camera.rotation.z === 0 && camera.rotation.y === Math.PI / 2) {
+			// Right //////////////////////////////////////////////////////////////////////////////
+			if (labelYNegative.current) labelYNegative.current.visible = true
+			if (lineYNegative.current) lineYNegative.current.visible = true
+
+			if (labelZNegative.current) labelZNegative.current.visible = true
+			if (lineZNegative.current) lineZNegative.current.visible = true
+		} else if (camera.rotation.z === 0 && camera.rotation.y === -Math.PI / 2) {
+			// Left ///////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = true
+			if (lineXNegative.current) lineXNegative.current.visible = true
+
+			if (labelYNegative.current) labelYNegative.current.visible = true
+			if (lineYNegative.current) lineYNegative.current.visible = true
+
+			if (labelZNegative.current) labelZNegative.current.visible = true
+			if (lineZNegative.current) lineZNegative.current.visible = true
+		} else if (camera.rotation.z === 0 && camera.rotation.y === 0) {
+			// Front //////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = true
+			if (lineXNegative.current) lineXNegative.current.visible = true
+
+			if (labelYNegative.current) labelYNegative.current.visible = true
+			if (lineYNegative.current) lineYNegative.current.visible = true
+
+			if (labelZNegative.current) labelZNegative.current.visible = true
+			if (lineZNegative.current) lineZNegative.current.visible = true
+		} else if (Math.abs(camera.rotation.z) === Math.PI && Math.abs(camera.rotation.y) < 0.001) {
+			// Back ///////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = true
+			if (lineXNegative.current) lineXNegative.current.visible = true
+
+			if (labelZNegative.current) labelZNegative.current.visible = true
+			if (lineZNegative.current) lineZNegative.current.visible = true
+		} else if (!isMouseEnter.current) {
+			// Null ///////////////////////////////////////////////////////////////////////////////
+			if (labelXNegative.current) labelXNegative.current.visible = false
+			if (lineXNegative.current) lineXNegative.current.visible = false
+
+			if (labelYNegative.current) labelYNegative.current.visible = false
+			if (lineYNegative.current) lineYNegative.current.visible = false
+
+			if (labelZNegative.current) labelZNegative.current.visible = false
+			if (lineZNegative.current) lineZNegative.current.visible = false
+		}
+	}, 1)
+
+	return (
+		<>
+			<group>
+				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#ff3653'} lineWidth={2} />
+				<Billboard position={[0.4, 0, 0]}>
+					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
+						X
+						<meshBasicMaterial ref={labelX} color={'black'} fog={false} />
+					</Text>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							labelX.current?.color.set('white')
+						}}
+						onPointerLeave={() => {
+							labelX.current?.color.set('black')
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickX) onClickX()
+						}}
+					>
+						<meshBasicMaterial color={'#ff3653'} />
+					</Circle>
+				</Billboard>
+			</group>
+			<group rotation-z={Math.PI * 0.5}>
+				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#77b316'} lineWidth={2} />
+				<Billboard position={[0.4, 0, 0]}>
+					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
+						Y
+						<meshBasicMaterial ref={labelY} color={'black'} fog={false} />
+					</Text>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							labelY.current?.color.set('white')
+						}}
+						onPointerLeave={() => {
+							labelY.current?.color.set('black')
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickY) onClickY()
+						}}
+					>
+						<meshBasicMaterial color={'#77b316'} />
+					</Circle>
+				</Billboard>
+			</group>
+			<group rotation-y={-Math.PI * 0.5}>
+				<Line fog points={[0, 0, 0, 0.31, 0, 0]} color={'#317acd'} lineWidth={2} />
+				<Billboard position={[0.4, 0, 0]}>
+					<Text position={[0, 0, 0.01]} font='/fonts/Inter-Bold.woff' fontSize={0.14}>
+						Z
+						<meshBasicMaterial ref={labelZ} color={'black'} fog={false} />
+					</Text>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							labelZ.current?.color.set('white')
+						}}
+						onPointerLeave={() => {
+							labelZ.current?.color.set('black')
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickZ) onClickZ()
+						}}
+					>
+						<meshBasicMaterial color={'#317acd'} />
+					</Circle>
+				</Billboard>
+			</group>
+			<group rotation-z={Math.PI}>
+				<Line
+					ref={lineXNegative}
+					points={[0, 0, 0, 0.31, 0, 0]}
+					color={'#ff3653'}
+					lineWidth={2}
+					depthTest={false}
+					depthWrite={false}
+				/>
+				<Billboard position={[0.4, 0, 0]}>
+					<group ref={labelXNegative} visible={false} position={[0, 0, 0.01]}>
+						<Text font='/fonts/Inter-Bold.woff' fontSize={0.14}>
+							-X
+							<meshBasicMaterial color={'white'} fog={false} />
+						</Text>
+					</group>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							if (labelXNegative.current) labelXNegative.current.visible = true
+							isMouseEnter.current = true
+						}}
+						onPointerLeave={() => {
+							if (labelXNegative.current) labelXNegative.current.visible = false
+							isMouseEnter.current = false
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickXNegative) onClickXNegative()
+						}}
+					>
+						<meshBasicMaterial color={'#ff3653'} />
+					</Circle>
+					<Circle scale={0.09} position={[0, 0, 0.005]}>
+						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
+					</Circle>
+				</Billboard>
+			</group>
+			<group rotation-z={-Math.PI / 2}>
+				<Line
+					ref={lineYNegative}
+					points={[0, 0, 0, 0.31, 0, 0]}
+					color={'#77b316'}
+					lineWidth={2}
+					depthTest={false}
+					depthWrite={false}
+				/>
+				<Billboard position={[0.4, 0, 0]}>
+					<group ref={labelYNegative} visible={false} position={[0, 0, 0.01]}>
+						<Text font='/fonts/Inter-Bold.woff' color={'white'} fontSize={0.14}>
+							-Y
+							<meshBasicMaterial color={'white'} fog={false} />
+						</Text>
+					</group>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							if (labelYNegative.current) labelYNegative.current.visible = true
+							isMouseEnter.current = true
+						}}
+						onPointerLeave={() => {
+							if (labelYNegative.current) labelYNegative.current.visible = false
+							isMouseEnter.current = false
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickYNegative) onClickYNegative()
+						}}
+					>
+						<meshBasicMaterial color={'#77b316'} />
+					</Circle>
+					<Circle scale={0.09} position={[0, 0, 0.005]}>
+						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
+					</Circle>
+				</Billboard>
+			</group>
+			<group rotation-y={Math.PI / 2}>
+				<Line
+					ref={lineZNegative}
+					points={[0, 0, 0, 0.31, 0, 0]}
+					color={'#317acd'}
+					lineWidth={2}
+					depthTest={false}
+					depthWrite={false}
+				/>
+				<Billboard position={[0.4, 0, 0]}>
+					<group ref={labelZNegative} visible={false} position={[0, 0, 0.01]}>
+						<Text font='/fonts/Inter-Bold.woff' color={'white'} fontSize={0.14}>
+							-Z
+							<meshBasicMaterial color={'white'} fog={false} />
+						</Text>
+					</group>
+					<Circle
+						scale={0.1}
+						onPointerEnter={(event) => {
+							event.stopPropagation()
+							if (labelZNegative.current) labelZNegative.current.visible = true
+							isMouseEnter.current = true
+						}}
+						onPointerLeave={() => {
+							if (labelZNegative.current) labelZNegative.current.visible = false
+							isMouseEnter.current = false
+						}}
+						onClick={(event) => {
+							event.stopPropagation()
+							if (onClickZNegative) onClickZNegative()
+						}}
+					>
+						<meshBasicMaterial color={'#317acd'} />
+					</Circle>
+					<Circle scale={0.09} position={[0, 0, 0.005]}>
+						<meshBasicMaterial color={'black'} transparent opacity={0.7} />
+					</Circle>
+				</Billboard>
+			</group>
+		</>
+	)
+}
