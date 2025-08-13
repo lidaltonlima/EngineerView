ca6c125f72e6d676ed294aadfbcd16d5aea9b64a
lidaltonlima <lidaltonlima@gmail.com>
2025-08-13 18:07:34 -0400

    update (Axes): use arrows element

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,6 +4,7 @@ import { Node, Bar } from './components/3d/objects'
 import { DrawSupport } from './components/3d/utils'
 import { useStructureContext } from './contexts/Structure'
 import { IStructureData } from './types/Structure'
+import { Axes } from './components/3d/objects/Axes'
 
 export const App = (): React.JSX.Element => {
 	const [structureData, setStructureData] = useState<IStructureData | null>()
@@ -30,6 +31,8 @@ export const App = (): React.JSX.Element => {
 			{structureData?.bars.map((bar) => <Bar key={bar.name} bar={bar} />)}
 			{structureData?.nodes.map((node) => <Node key={node.name} node={node} />)}
 			{structureData?.supports.map((support) => DrawSupport(support))}
+
+			<Axes />
 		</Default3dScene>
 	)
 }
--- src/renderer/src/components/3d/objects/Axes/Axes.tsx
+++ src/renderer/src/components/3d/objects/Axes/Axes.tsx
@@ -1,4 +1,5 @@
-import { Billboard, Line, Text } from '@react-three/drei'
+import { Billboard, Text } from '@react-three/drei'
+import { Arrow } from '../Arrow'
 
 interface IAxesProps {
 	position?: [number, number, number]
@@ -20,60 +21,27 @@ export const Axes = ({
 	position = [0, 0, 0],
 	rotation = [0, 0, 0],
 	scale = 1,
-	label = false
+	label = true
 }: IAxesProps): React.JSX.Element => {
 	return (
 		<group rotation={rotation} position={position} scale={scale}>
-			<group rotation-x={Math.PI / 2}>
-				<mesh position={[0, 1, 0]}>
-					<coneGeometry args={[0.15, 0.5, 32]} />
-					<meshBasicMaterial color={'blue'} />
-				</mesh>
-				<Line
-					worldUnits
-					points={[0, 0, 0, 0, 1, 0]}
-					lineWidth={0.1 * scale}
-					color={'blue'}
-				/>
-			</group>
-			<group>
-				<mesh position={[0, 1, 0]}>
-					<coneGeometry args={[0.15, 0.5, 32]} />
-					<meshBasicMaterial color={'green'} />
-				</mesh>
-				<Line
-					worldUnits
-					points={[0, 0, 0, 0, 1, 0]}
-					lineWidth={0.1 * scale}
-					color={'green'}
-				/>
-			</group>
-			<group rotation-z={-Math.PI / 2}>
-				<mesh position={[0, 1, 0]}>
-					<coneGeometry args={[0.15, 0.5, 32]} />
-					<meshBasicMaterial color={'red'} />
-				</mesh>
-				<Line
-					worldUnits
-					points={[0, 0, 0, 0, 1, 0]}
-					lineWidth={0.1 * scale}
-					color={'red'}
-				/>
-			</group>
+			<Arrow />
+			<Arrow direction='y' color='green' />
+			<Arrow direction='z' color='blue' />
 			{label && (
 				<>
-					<Billboard position={[1.5, 0, 0]}>
-						<Text fontSize={0.5} font={'/fonts/Inter-Bold.woff'}>
+					<Billboard position={[1.2, 0, 0]}>
+						<Text fontSize={0.4} font={'/fonts/Inter-Bold.woff'}>
 							X
 						</Text>
 					</Billboard>
-					<Billboard position={[0, 1.5, 0]}>
-						<Text fontSize={0.5} font={'/fonts/Inter-Bold.woff'}>
+					<Billboard position={[0, 1.2, 0]}>
+						<Text fontSize={0.4} font={'/fonts/Inter-Bold.woff'}>
 							Y
 						</Text>
 					</Billboard>
-					<Billboard position={[0, 0, 1.5]}>
-						<Text fontSize={0.5} font={'/fonts/Inter-Bold.woff'}>
+					<Billboard position={[0, 0, 1.2]}>
+						<Text fontSize={0.4} font={'/fonts/Inter-Bold.woff'}>
 							Z
 						</Text>
 					</Billboard>
